bf7431aa3eb88e4ce4fa9e1a0d4c9f3ac5e2c3e9
Max-Kucher-Sar <kucherenko.max75@mail.ru>
2024-10-21 22:50:58 +0400

    Add files via upload

--- module_10_2.py
+++ module_10_2.py
@@ -16,28 +16,16 @@ class Knight(Thread):
             enemies = enemies - self.power
             time.sleep(1)
             day_count += 1
-            print(f'{self.name} сражается {day_count} день(дня), осталось {enemies} воинов.')
+            print(f'{self.name} сражается {day_count} день(дня), осталось {enemies} воинов.\n')
         print(f'{self.name} одержал победу спустя {day_count} дней(дня)!')
 
 first_knight = Knight('Sir Lancelot', 10)
 second_knight = Knight("Sir Galahad", 20)
-
-"""for _ in range(2):
-    first_knight.start()
-    second_knight.start()
-for _ in range(2):
-    first_knight.join()
-    second_knight.join()
-    Почему-то когда запускаю потоки через цикл, вылетает ошибка RuntimeError if called more than once,
-    а в случае с рабочим кодом ниже все нормально (за исключением отсутствия переноса строки в одном месте),
-     хотя судя по консоли они запускаются в одно время
-    """
-first_knight.start()
-second_knight.start()
-
-first_knight.join()
-second_knight.join()
-
+thread_list = [first_knight, second_knight]
+for thread in thread_list:
+    thread.start()
+for thread in thread_list:
+    thread.join()
 
 
 print('Все битвы закончились!')
